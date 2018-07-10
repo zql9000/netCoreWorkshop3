5f5c47ef92df0129ec93686f2c062b20da040236
Ezequiel López <zql9000@users.noreply.github.com>
2018-07-10 12:20:52 -0300

    Primer commit

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace netCoreWorkshop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var startupLogger = loggerFactory.CreateLogger<Startup>(); 
+
+            startupLogger.LogTrace("Esto es TRACE");
+            startupLogger.LogDebug("Esto es DEBUG");
+            startupLogger.LogInformation("Esto es INFORMATION");
+            startupLogger.LogError("Esto es ERROR");
+            startupLogger.LogCritical("Esto es CRITICAL");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
